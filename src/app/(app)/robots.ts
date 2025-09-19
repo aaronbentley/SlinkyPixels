ddc36c3e87a02bf77b1fedbdd3b577face64268a
aaronbentley <aaronbentley@me.com>
2025-09-19 17:16:02 +0100

    refactor: simplify robots function syntax

--- src/app/(app)/robots.ts
+++ src/app/(app)/robots.ts
@@ -3,15 +3,13 @@
  */
 import { MetadataRoute } from 'next'
 
-const robots = (): MetadataRoute.Robots => {
-    return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: ['/studio/*']
-        },
-        sitemap: `${process.env.APP_URL!}/sitemap.xml`
-    }
-}
+const robots = (): MetadataRoute.Robots => ({
+    rules: {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/studio/*']
+    },
+    sitemap: `${process.env.APP_URL!}/sitemap.xml`
+})
 
 export default robots

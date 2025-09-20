ad88282a8d2325c503d0271c2db5c6e901e1353c
aaronbentley <aaronbentley@me.com>
2025-09-20 16:30:32 +0100

    fix: update content rendering logic in CollectionGrid component and adjust proseBaseClasses in typography

--- src/components/content/collection-grid.tsx
+++ src/components/content/collection-grid.tsx
@@ -54,58 +54,59 @@ const CollectionGrid = async ({
             className={cn(['container', 'lg:max-w-[980px]'])}>
             <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
                 {content &&
-                    (limit ? content.slice(0, limit) : content).map(
-                        (contentItem) => {
-                            /**
-                             * If no content item, return null
-                             */
-                            if (!contentItem) return null
+                    (contentType !== 'custom' && limit
+                        ? content.slice(0, limit)
+                        : content
+                    ).map((contentItem) => {
+                        /**
+                         * If no content item, return null
+                         */
+                        if (!contentItem) return null
 
-                            /**
-                             * If no content item slug, return null
-                             */
-                            if (!contentItem.slug || !contentItem.slug.current)
-                                return null
+                        /**
+                         * If no content item slug, return null
+                         */
+                        if (!contentItem.slug || !contentItem.slug.current)
+                            return null
 
-                            /**
-                             * Resolve document reference URL
-                             */
-                            const href = resolveDocumentReferenceURL(
-                                contentItem._type,
-                                contentItem.slug
-                            )
+                        /**
+                         * Resolve document reference URL
+                         */
+                        const href = resolveDocumentReferenceURL(
+                            contentItem._type,
+                            contentItem.slug
+                        )
 
-                            return (
-                                <Link
-                                    key={contentItem._id}
-                                    href={href}
-                                    title={`View ${contentItem.title}`}
-                                    className='group'>
-                                    <Card className='h-full overflow-hidden border-2 pt-0 transition-colors duration-200 group-hover:border-secondary-foreground'>
-                                        {contentItem.image && (
-                                            <Image
-                                                image={contentItem.image}
-                                                alt={contentItem.title}
-                                                width={480}
-                                                height={320}
-                                                className='aspect-4/3 object-cover data-[lqip=true]:aspect-4/3!'
-                                            />
-                                        )}
-                                        <CardHeader>
-                                            <CardTitle>
-                                                {contentItem.title}
-                                            </CardTitle>
-                                        </CardHeader>
-                                        <CardContent>
-                                            <CardDescription>
-                                                {contentItem.subtitle}
-                                            </CardDescription>
-                                        </CardContent>
-                                    </Card>
-                                </Link>
-                            )
-                        }
-                    )}
+                        return (
+                            <Link
+                                key={contentItem._id}
+                                href={href}
+                                title={`View ${contentItem.title}`}
+                                className='group'>
+                                <Card className='h-full overflow-hidden border-2 pt-0 transition-colors duration-200 group-hover:border-secondary-foreground'>
+                                    {contentItem.image && (
+                                        <Image
+                                            image={contentItem.image}
+                                            alt={contentItem.title}
+                                            width={480}
+                                            height={320}
+                                            className='aspect-4/3 object-cover data-[lqip=true]:aspect-4/3!'
+                                        />
+                                    )}
+                                    <CardHeader>
+                                        <CardTitle>
+                                            {contentItem.title}
+                                        </CardTitle>
+                                    </CardHeader>
+                                    <CardContent>
+                                        <CardDescription>
+                                            {contentItem.subtitle}
+                                        </CardDescription>
+                                    </CardContent>
+                                </Card>
+                            </Link>
+                        )
+                    })}
             </div>
         </div>
     )
